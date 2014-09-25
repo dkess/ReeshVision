dea8d419ba40117627f94c5f8835b75607b4aa1c
Daniel Kessler <sinani201@gmail.com>
2014-09-24 18:07:28 -0700

    fixed lag issues when disconnected from robot

--- reesh.py
+++ reesh.py
@@ -136,23 +136,22 @@ if __name__ == "__main__":
             if not connected:
                 try:
                     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    s.settimeout(.5)
+                    s.settimeout(.1)
                     s.connect(("10.18.36.2", 1180))
                     connected = True
-                except:
+                except socket.timeout:
                     print "failed to connect"
                     last_t = cur_time + 1000
-            else:
-                try:
-                    write_bytes = bytearray()
-                    write_bytes.append(byte_to_send)
-                    s.send(write_bytes)
-                    print byte_to_send
-                    last_t = cur_time
-                    connected = True
-                except socket.timeout:
-                    print "timed out"
-                    connected = False
+            try:
+                write_bytes = bytearray()
+                write_bytes.append(byte_to_send)
+                s.send(write_bytes)
+                print byte_to_send
+                last_t = cur_time
+                connected = True
+            except socket.timeout:
+                print "timed out"
+                connected = False
 
         # Flip the image so the the screen is like a mirror
         wc_img = cv.flip(cv.resize(frame, (config["webcam_w"], config["webcam_h"])), 1)
